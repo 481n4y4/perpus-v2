d8086e08c453cdeb7c27b2c0b17e320f5dc3385e
481n4y4 <kheiraabinaya14@gmail.com>
2026-06-11 14:45:37 +0700

    📌 chore(api): making crud

--- backend/main.go
+++ backend/main.go
@@ -1,17 +1,28 @@
 package main
 
 import (
+	"backend/controllers"
+	"backend/models"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	router := gin.Default()
 
+	models.ConnectDatabase()
+
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "Hello World!",
 		})
 	})
+
+	router.GET("/api/posts", controllers.FindPosts)
+	router.POST("/api/posts", controllers.StorePost)
+	router.GET("/api/posts/:id", controllers.FindPostsById)
+	router.PUT("/api/posts/:id", controllers.UpdatePost)
+	router.DELETE("/api/posts/:id", controllers.DeletePost)
 
 	router.Run(":3000")
 }
