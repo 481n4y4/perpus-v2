ac8bf72d962286285b89108f5e32d260651d7073
481n4y4 <kheiraabinaya14@gmail.com>
2026-06-12 14:29:37 +0700

    ✨ feat(API): create a route

--- backend/main.go
+++ backend/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"backend/controllers"
+	"backend/middleware"
 	"backend/models"
 
 	"github.com/gin-gonic/gin"
@@ -17,12 +18,33 @@ func main() {
 			"message": "Hello World!",
 		})
 	})
-
-	router.GET("/api/books", controllers.FindBooks)
-	router.POST("/api/books", controllers.StoreBook)
-	router.GET("/api/books/:id", controllers.FindBookById)
-	router.PUT("/api/books/:id", controllers.UpdateBook)
-	router.DELETE("/api/books/:id", controllers.DeleteBook)
+	
+	// Auth
+	authApi:= router.Group("/api")
+	{
+		authApi.POST("/auth/register", controllers.Register)
+		authApi.POST("/auth/login", controllers.Login)
+		authApi.POST("/auth/logout", controllers.Logout)
+	}
+	// router.POST("/api/auth/register", controllers.Register)
+	// router.POST("/api/auth/login", controllers.Login)
+	
+	
+	// CRUD Books
+	bookApi := router.Group("/api")
+	bookApi.Use(middleware.RequireAuth())
+	{
+		bookApi.GET("/books", controllers.FindBooks)
+		bookApi.POST("/books", controllers.StoreBook)
+		bookApi.GET("/books/:id", controllers.FindBookById)
+		bookApi.PUT("/books/:id", controllers.UpdateBook)
+		bookApi.DELETE("/books/:id", controllers.DeleteBook)
+	}
+	// router.GET("/api/books", controllers.FindBooks)
+	// router.POST("/api/books", controllers.StoreBook)
+	// router.GET("/api/books/:id", controllers.FindBookById)
+	// router.PUT("/api/books/:id", controllers.UpdateBook)
+	// router.DELETE("/api/books/:id", controllers.DeleteBook)
 
 	router.Run(":3000")
 }
