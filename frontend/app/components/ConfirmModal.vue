a98f946d422f17c227337d6804bf1378a761a947
481n4y4 <kheiraabinaya14@gmail.com>
2026-06-15 09:37:53 +0700

    ✨ feat(ui): create components

---
+++ frontend/app/components/ConfirmModal.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="fixed inset-0 z-50 flex items-center justify-center bg-black/50">
+    <div
+      class="w-full max-w-md rounded-xl bg-gray-800 p-6 text-white shadow-lg"
+    >
+      <!-- Title -->
+      <h2 class="text-xl font-bold">Confirm Delete</h2>
+
+      <!-- Message -->
+      <p class="mt-3 text-gray-300">
+        Are you sure you want to delete this book?
+      </p>
+
+      <!-- Actions -->
+      <div class="mt-6 flex justify-end gap-3">
+        <button
+        @click="emit('close')"
+          class="rounded bg-gray-600 px-4 py-2 font-semibold hover:bg-gray-500"
+        >
+          Cancel
+        </button>
+
+        <button
+        @click="emit('confirm')"
+          class="rounded bg-red-600 px-4 py-2 font-semibold hover:bg-red-500"
+        >
+          Delete
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const emit = defineEmits(['close', 'confirm'])
+</script>
---
+++ frontend/app/components/Navbar.vue
@@ -0,0 +1,65 @@
+<template>
+  <nav
+    class="fixed z-50 bg-gray-900 flex items-center w-screen justify-between p-6 lg:px-8"
+  >
+    <div class="flex lg:hidden">
+      <button
+        type="button"
+        command="show-modal"
+        commandfor="mobile-menu"
+        class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5"
+      >
+        <span class="sr-only">Open main menu</span>
+        <svg
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="1.5"
+          data-slot="icon"
+          aria-hidden="true"
+          class="size-6"
+        >
+          <path
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </button>
+    </div>
+    <div class="lg:flex lg:gap-x-12">
+      <NuxtLink to="/" class="text-sm/6 font-semibold text-white"
+        >{{ item1 }}</NuxtLink
+      >
+      <NuxtLink to="/about" class="text-sm/6 font-semibold text-white"
+        >{{item2}}</NuxtLink
+      >
+      <NuxtLink to="/books" class="text-sm/6 font-semibold text-white"
+        >{{ item3 }}</NuxtLink
+      >
+    </div>
+    <div class="lg:flex lg:flex-1 lg:justify-end lg:gap-x-12">
+      <NuxtLink
+        v-if="token"
+        to="/profile"
+        class="text-sm/6 font-semibold text-white"
+        >Profile</NuxtLink
+      >
+      <NuxtLink
+        v-else
+        to="/auth/signin"
+        class="text-sm/6 font-semibold text-white"
+        >Log in <span aria-hidden="true">&rarr;</span></NuxtLink
+      >
+    </div>
+  </nav>
+</template>
+
+<script setup>
+defineProps({
+  item1: String,
+  item2: String,
+  item3: String
+})
+const token = useCookie("auth_token");
+</script>
