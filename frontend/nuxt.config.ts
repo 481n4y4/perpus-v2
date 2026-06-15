2d51ffd13a1b8b4fe6da100835e471cec7bc1ac3
481n4y4 <kheiraabinaya14@gmail.com>
2026-06-15 09:35:52 +0700

    ⚙️ config(config): setting up the nuxt js config

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/tailwindcss'
+  ],
+  runtimeConfig: {
+    public: {
+      apiBase: "http://127.0.0.1:3000/api",
+    },
+  },
 })
