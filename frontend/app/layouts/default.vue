69088f4483339ceda2d0fa762fc3eab887d27960
481n4y4 <kheiraabinaya14@gmail.com>
2026-06-15 09:38:20 +0700

    ✨ feat(ui): create layouts

---
+++ frontend/app/layouts/default.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex flex-col justify-between bg-gray-900 min-h-screen">
+    <Navbar item1="Home" item2="About" item3="Book Lists" />
+    <slot />
+    <Footer />
+  </div>
+</template>
